9b175cf532d70b2bdbf7acccf3d41f123e06322f
jupi <leivadiazjulio@gmail.com>
2017-07-14 22:06:20 +0200

    SetUp

---
+++ src/app/shared/data-storage.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import 'rxjs/Rx';
+
+import { RecipeService } from '../recipes/recipe.service';
+import { Recipe } from '../recipes/recipe.model';
+
+@Injectable()
+export class DataStorageService {
+  constructor(private http: Http, private recipeService: RecipeService) {}
+
+  storeRecipes() {
+    return this.http.put('https://recipes-4dd95.firebaseio.com/recipes.json', this.recipeService.getRecipes());
+  }
+
+  getRecipes() {
+    this.http.get('https://recipes-4dd95.firebaseio.com/recipes.json')
+      .map(
+        (response: Response) => {
+          const recipes: Recipe[] = response.json();
+          for (const recipe of recipes) {
+            if (!recipe['ingredients']) {
+              recipe['ingredients'] = [];
+              console.log('Aquí');
+            }
+          }
+          return recipes;
+        }
+      )
+      .subscribe(
+        (recipes: Recipe[]) => {
+          this.recipeService.setRecipes(recipes);
+        }
+      );
+  }
+}
